46a34137f1ba6a2a94602d224d07f4bbada6e53c
Myan Ngo <133174850+myanngo@users.noreply.github.com>
2025-05-23 14:13:12 -0700

    finalized dialogue system

--- Assets/Scripts/NPC/NPCInteractable.cs
+++ Assets/Scripts/NPC/NPCInteractable.cs
@@ -1,34 +1,56 @@
 using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
 
 public class NPCInteractable : MonoBehaviour
 {
     [SerializeField] private string npcName;
-    [TextArea(2, 4)]
-    [SerializeField] private List<string> dialogueLines;
+
+    private int dialogueStage = 0; // 0 = intro, 1 = glass shard mission, etc.
 
     public void Interact()
     {
-         List<string> lines;
+        List<string> lines = new List<string>();
 
-    if (InventoryManager.Instance.CountItem(ItemType.GlassPiece) >= 14)
-    {
-        lines = new List<string>
+        if (dialogueStage == 0)
         {
-            "Wow! Look at you, you little scavenger. Let’s see if we can piece something together with these …"
-        };
+            lines = new List<string>
+            {
+                "Excuse me, you spry, young thing. Could I ask for your help?",
+                "The Great Tree, which is home to many of the forest’s creatures, is sick!",
+                "All the pollutants have infected its roots and are spreading up its trunk.",
+                "Luckily, I think I know a salve that will do the trick. But, I need your help collecting the materials. Would you help me and the forest?",
+                "Thank you, sweetie. First, we’ll need some kind of bottle in which to make the elixir."
+            };
 
-        // Launch puzzle logic here too
-    }
-    else
-    {
-        lines = new List<string>
+            dialogueStage = 1; // advance to next stage
+        }
+        else if (dialogueStage == 1)
         {
-            "We might as well make the best of a bad situation...",
-            "Go collect all of the glass you can find — there should be 14 shards!"
-        };
-    }
-        DialogueManager.Instance.ShowDialogue(npcName, dialogueLines);
+            if (InventoryManager.Instance.CountItem(ItemType.GlassPiece) >= 14)
+            {
+                lines = new List<string>
+                {
+                    "We might as well make the best of a bad situation...",
+                    "Go collect all of the glass you can find — there should be 14 shards!"
+                };
+            }
+            else
+            {
+                lines = new List<string>
+                {
+                    "Wow! Look at you, you little scavenger.",
+                    "Let’s see if we can piece something together with these …"
+                };
+
+                dialogueStage = 2;
+                UnityEngine.SceneManagement.SceneManager.LoadScene("PuzzleInterface");
+            }
+        }
+        else
+        {
+            lines = new List<string> { "Thanks again for your help, sweetie!" };
+        }
+
+        DialogueManager.Instance.ShowDialogue(npcName, lines);
     }
 }
