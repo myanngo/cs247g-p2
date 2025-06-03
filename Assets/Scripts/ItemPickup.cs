b3df917cc3e5931d8931ec22e378cea2be4f1783
Myan Ngo <133174850+myanngo@users.noreply.github.com>
2025-06-03 10:24:00 -0700

    integration start

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -6,10 +6,8 @@ public class ItemPickup : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other) 
     {
-        Debug.Log("hhhh");
         if (other.CompareTag("Player"))
         {
-            Debug.Log("colliddss");
             InventoryManager inventory = FindObjectOfType<InventoryManager>();
             if (inventory != null && inventory.AddItem(itemData))
             {
